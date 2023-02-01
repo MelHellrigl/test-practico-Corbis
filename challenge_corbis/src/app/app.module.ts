77c883d93996cb87d16ab197c46011f9516021a5
MelHellrigl <hellriglandreamelissa@gmail.com>
2023-02-01 19:18:44 -0300

    code/toolbar & sidebar

--- challenge_corbis/src/app/app.module.ts
+++ challenge_corbis/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
