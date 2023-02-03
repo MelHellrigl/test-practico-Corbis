bb4a2f73c65f56de7abd6d73ff188e5cbe58eef6
MelHellrigl <hellriglandreamelissa@gmail.com>
2023-02-03 09:52:32 -0300

    style

--- challenge_corbis/src/app/pipes/search.pipe.ts
+++ challenge_corbis/src/app/pipes/search.pipe.ts
@@ -6,10 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
   transform(list: any[], text: string): any {
     if (!text) return list;
-    return list.filter(
-      (res) =>
-        res.name.toUpperCase().includes(text.toUpperCase()) ||
-        res.perspective.name.toUpperCase().includes(text.toUpperCase())
+    return list.filter((res) =>
+      res.name.toUpperCase().includes(text.toUpperCase())
     );
   }
 }
